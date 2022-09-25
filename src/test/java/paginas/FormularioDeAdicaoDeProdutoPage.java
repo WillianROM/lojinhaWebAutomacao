4be0720bd4a8a9aebe204552a175604e2059bb1d
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2022-09-25 14:50:00 -0300

    Update FormularioDeAdicaoDeProdutoPage.java

--- src/test/java/paginas/FormularioDeAdicaoDeProdutoPage.java
+++ src/test/java/paginas/FormularioDeAdicaoDeProdutoPage.java
@@ -1,5 +1,6 @@
 package paginas;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class FormularioDeAdicaoDeProdutoPage {
@@ -8,4 +9,31 @@ public class FormularioDeAdicaoDeProdutoPage {
     public FormularioDeAdicaoDeProdutoPage(WebDriver driver) {
         this.driver = driver;
     }
+
+    public FormularioDeAdicaoDeProdutoPage informarNomeDoProduto(String produtoNome){
+        this.driver.findElement(By.id("produtonome")).sendKeys(produtoNome);
+        return this;
+    }
+
+    public FormularioDeAdicaoDeProdutoPage informarValorDoProduto(String produtoValor){
+        this.driver.findElement(By.name("produtovalor")).sendKeys(produtoValor);
+        return this;
+    }
+
+    public FormularioDeAdicaoDeProdutoPage informarCoresDoProduto(String produtoCores){
+        this.driver.findElement(By.id("produtocores")).sendKeys(produtoCores);
+        return this;
+    }
+
+    public ListaDeProdutosPage submeterFormularioDeAdicaoComErro(){
+        this.driver.findElement(By.cssSelector("button[type='submit']")).click();
+        return new ListaDeProdutosPage(this.driver);
+    }
+
+    public FormularioDeEdicaoDeProdutoPage submeterFormularioDeAdicaoComSucesso(){
+        this.driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        return new FormularioDeEdicaoDeProdutoPage(this.driver);
+    }
+
 }
