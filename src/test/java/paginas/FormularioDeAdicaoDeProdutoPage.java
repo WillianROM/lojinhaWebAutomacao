60ebe54ec208cc52741121f0fa213abc90b0e08a
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2022-09-25 09:52:52 -0300

    Create FormularioDeAdicaoDeProdutoPage.java

---
+++ src/test/java/paginas/FormularioDeAdicaoDeProdutoPage.java
@@ -0,0 +1,11 @@
+package paginas;
+
+import org.openqa.selenium.WebDriver;
+
+public class FormularioDeAdicaoDeProdutoPage {
+    private WebDriver driver;
+
+    public FormularioDeAdicaoDeProdutoPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
